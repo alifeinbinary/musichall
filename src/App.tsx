fba63fa7676c82ea4609197b4a238d364f6fb0cf
Andrew Halliwell <studio@strangeways.ca>
2023-07-28 15:45:47 -0700

    Adding release links

--- src/App.tsx
+++ src/App.tsx
@@ -12,29 +12,36 @@ function App() {
         <div className="flex flex-col justify-center items-center">
           <img className='h-auto rounded-lg drop-shadow-lg' src="../vertical_bird-screaming-800x1000.webp" alt="bird screaming cover" width={"800px"} height={"1000px"} />
         </div>
-        <div className="flex flex-col justify-end text-center md:text-left md:mb-8 mt-8 pl-0 md:pl-8">
+        <div className="flex flex-col justify-end text-center align-middle md:text-left md:mb-4 mt-4 pl-0 md:pl-8">
 
-          <h1 className="text-5xl font-extrabold">New album <br />'Bird Screaming' <br />
-            <small className="ml-2 font-semibold text-gray-500">out 28 July 2023</small>
+          <h1 className="text-5xl font-extrabold">New album <br />Bird Screaming<br />
+            <small className="font-semibold text-gray-500">out now!</small>
           </h1>
-          <div className="social-icons">
+          <div className="purchase-links">
+            <span className='font-bold align-middle mr-4'>Buy</span>
+            <a aria-label='Buy music from Music Hall on Bandcamp' href="https://musichallband.bandcamp.com/">
+              <button aria-label='Bandcamp' className="btn btn-primary"><FontAwesomeIcon icon={faBandcamp} /></button>
+            </a>
+          </div>
+          <div className="streaming-links">
+            <span className='font-bold align-middle mr-4'>Stream</span>
             <a aria-label='Listen to Music Hall on Spotify' href="https://open.spotify.com/artist/0awumhwRVHMdvFZL2pZxOx?si=cxN6APKzQ-SFqPu-WQrS6w">
               <button aria-label='Spotify' className="btn btn-primary"><FontAwesomeIcon icon={faSpotify} /></button>
             </a>
             <a aria-label='Listen to Music Hall on Apple Music' href="https://music.apple.com/us/artist/music-hall/1652906897">
               <button aria-label='Apple Music' className="btn btn-primary"><FontAwesomeIcon icon={faApple} /></button>
             </a>
-            <a aria-label='Buy music from Music Hall on Bandcamp' href="https://alifeinbinary.bandcamp.com/album/let-the-night-disappear">
-              <button aria-label='Bandcamp' className="btn btn-primary"><FontAwesomeIcon icon={faBandcamp} /></button>
-            </a>
             <a aria-label='Listen to Music Hall on Youtube' href="https://www.youtube.com/channel/UCZhiDBOQ2Pu-3YDmdbfGd8g">
               <button aria-label='Youtube' className="btn btn-primary"><FontAwesomeIcon icon={faYoutube} /></button>
             </a>
-            <a aria-label='Follow Music Hall on TikTok' href="https://www.tiktok.com/@musichall.band">
-              <button aria-label="TikTok" className="btn btn-primary"><FontAwesomeIcon icon={faTiktok} /></button>
-            </a>
+          </div>
+          <div className="social-icons">
+            <span className='font-bold align-middle mr-4'>Follow</span>
             <a aria-label='Follow Music Hall on Instagram' href="https://www.instagram.com/musichallband/">
               <button aria-label='Instagram' className="btn btn-primary"><FontAwesomeIcon icon={faInstagram} /></button>
+            </a>
+            <a aria-label='Follow Music Hall on TikTok' href="https://www.tiktok.com/@musichall.band">
+              <button aria-label="TikTok" className="btn btn-primary"><FontAwesomeIcon icon={faTiktok} /></button>
             </a>
           </div>
           <div className="mt-4 contact">
